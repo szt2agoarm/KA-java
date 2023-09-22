b02e83a035162d7371c234d14ddba225762690d6
agocsarmin <115459668+szt2agoarm@users.noreply.github.com>
2023-09-22 20:00:05 +0200

    Add files via upload

--- java/DrawChessTable.java
+++ java/DrawChessTable.java
@@ -2,27 +2,22 @@ import java.util.Scanner;
 
 public class DrawChessTable {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter the chess table size: ");
         int size = scanner.nextInt();
 
-        for (int row = 1; row <= size; row++)
-        {
-            for (int col = 1; col <= size; col++)
-            {
-                if ((row + col) % 2 == 0)
-                {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if ((i + j) % 2 == 0) {
                     System.out.print("% ");
-                }
-                else
-                {
+                } else {
                     System.out.print("  ");
                 }
             }
             System.out.println();
         }
+
         scanner.close();
     }
 }
