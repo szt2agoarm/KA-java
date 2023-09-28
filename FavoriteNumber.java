427ec0c3f1d8af84b73635f0ffb9cc2e9e0b3bc5
agocsarmin <115459668+szt2agoarm@users.noreply.github.com>
2023-09-28 07:32:04 +0200

    Add files via upload

---
+++ FavoriteNumber.java
@@ -0,0 +1,7 @@
+public class FavoriteNumber{
+    public static void main(String[] args) {
+        int favoriteNumber = 15;
+
+        System.out.println("My favorite number is: " + favoriteNumber + ".");
+    }
+}
